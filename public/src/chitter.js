956721c4893d69ec03a35e583aaa12a9848a2adb
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-24 17:12:39 +0100

    Refactored to only require one api call

--- public/src/chitter.js
+++ public/src/chitter.js
@@ -4,15 +4,11 @@ class Peep {
   }
 }
 
-Peep.getText = async (i) => {
-  // console.log("inside getText")
-  return fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
+Peep.getText = () => {
+  fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
         .then(x => x.json()).then(x=> {
-              // for(var i = 0; i < x.length; i++) {
-                // allPeeps.innerHTML += x[i]['body'] + `</br>`
-                // console.log('hiya')
-                // console.log(new Peep(x[i]['body']))
-                return new Peep(x[i]['body'])
-              // }
+              for(var i = 0; i < x.length; i++) {
+                allPeeps.innerHTML += x[i]['body'] + `</br>`
+              }
           })
 }
--- public/src/interface.js
+++ public/src/interface.js
@@ -1,26 +1,10 @@
-// let peep = new Peep
 let allPeeps = document.querySelector('#peeps-all');
 let peepsButton = document.getElementById('peep')
 
 peepsButton.addEventListener("click", async () => {
-  getPeepsfromPeeps()
+  peepTutorial()
 })
 
-function getPeeps() {
-  return fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
-        .then(x => x.json()).then(x=> {
-              for(var i = 0; i < x.length; i++) {
-                allPeeps.innerHTML += x[i]['body'] + `</br>`
-              }
-          })
-}
-// getPeeps()
-
-async function getPeepsfromPeeps() {
-  // var peeps = []
-  // peeps.push(Peep.getText())
-  // return peeps
-  for (var i = 0; i < 51; i ++) {
-    Peep.getText(i).then((x) => allPeeps.innerHTML += x.text)
-  }
+function peepTutorial() {
+  Peep.getText()
 }
