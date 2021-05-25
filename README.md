112f3b68099079504c084fc5cece076c7875bb97
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-25 14:17:13 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chitter-api-challenge
+
+This is the chitter api challenge from [Makers Academy](https://github.com/Makers-Academy), in which the backend is hosted on Heroku [here](https://chitter-backend-api-v2.herokuapp.com/), and the task is to build an API connection and front end to interact with the backend with various http requests.
+
+The main purpose of this for me is to get more familiar with fetch, promises, and async functions.
--- public/src/interface.js
+++ public/src/interface.js
@@ -1,27 +1,35 @@
 let allPeeps = document.querySelector('#peeps-all');
-let peepsButton = document.getElementById('peep')
+let peepButton = document.getElementById('peep')
 
-// peepsButton.addEventListener("click", async () => {
-// }
 
 document.addEventListener('DOMContentLoaded', () => {
   getPeeps()
-  function getPeeps() {
-    var peepArray = []
-    fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
-          .then(
-            function(response){
-              if(response.status !== 200) {
-                allPeeps.innerHTML += 'Sorry, the api is not responding'
-              }
-              response.json().then(x=>{
-                x.forEach (peep => {
-                  allPeeps.innerHTML += peep.body + `</br>`
-                })
-              })
-          })
-          .catch(function(err){
-            allPeeps.innerHTML += ('Error :(')
-          })
-  }
+
+
+
+  peepButton.addEventListener("click", () => {
+    console.log("Iyaaaa")
+  })
 })
+
+function postPeep() {
+
+}
+function getPeeps() {
+  var peepArray = []
+  fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
+        .then(
+          function(response){
+            if(response.status !== 200) {
+              allPeeps.innerHTML += 'Sorry, the api is not responding'
+            }
+            response.json().then(x=>{
+              x.forEach (peep => {
+                allPeeps.innerHTML += peep.body + `</br>`
+              })
+            })
+        })
+        .catch(function(err){
+          allPeeps.innerHTML += ('Error :(')
+        })
+}
