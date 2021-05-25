f30bd9e95912221bcf6700f92127ad419905d328
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-25 13:58:34 +0100

    Cleaned up & refactored

--- public/src/interface.js
+++ public/src/interface.js
@@ -2,11 +2,26 @@ let allPeeps = document.querySelector('#peeps-all');
 let peepsButton = document.getElementById('peep')
 
 // peepsButton.addEventListener("click", async () => {
-//   peepTutorial()
-// })
-
-// function peepTutorial() {
-//   Peep.getText()
 // }
 
-Peep.getText()
+document.addEventListener('DOMContentLoaded', () => {
+  getPeeps()
+  function getPeeps() {
+    var peepArray = []
+    fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
+          .then(
+            function(response){
+              if(response.status !== 200) {
+                allPeeps.innerHTML += 'Sorry, the api is not responding'
+              }
+              response.json().then(x=>{
+                x.forEach (peep => {
+                  allPeeps.innerHTML += peep.body + `</br>`
+                })
+              })
+          })
+          .catch(function(err){
+            allPeeps.innerHTML += ('Error :(')
+          })
+  }
+})
